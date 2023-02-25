843e5d061ed95ba55e08cee6d8a92b79edc6aeb2
Administrator <Administrator@DESKTOP-02IVE87>
2023-02-25 11:31:27 +0800

    添加项目文件。

---
+++ 转义字符/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 转义字符
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //转义字符
+            string c = "He said,\"I'm tired now";
+            Console.WriteLine(c);
+            char c2 = '\'';
+            Console.WriteLine(c2);
+            string towLinseString = "The first-Line sentence.\nThe second-Line sectence.";
+            Console.WriteLine(towLinseString);
+            string s2 = "abc\\def";
+            Console.WriteLine(s2);
+            //string wrongString="\%";
+            //原义字符串
+            string verbatimString = @"The first-Line sentence.The second-Line sentence.";
+            Console.WriteLine(verbatimString);
+        }
+    }
+}
